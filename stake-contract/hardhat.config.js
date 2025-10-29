60be4617f2c91c035f43258f98ccc78e15ef4137
lc3091 <309138912@qq.com>
2025-10-29 11:12:16 +0800

    u

--- stake-contract/hardhat.config.js
+++ stake-contract/hardhat.config.js
@@ -14,15 +14,13 @@ module.exports = {
     },
   },
   networks: {
-    hardhat: {},
-    sepolia: {
-      url:
-        "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
-      accounts: [process.env.PRIVATE_KEY],
-      gasPrice: 30000000000, // 30 Gwei
-    },
+    // sepolia: {
+    //   url: "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
+    //   accounts: [process.env.PRIVATE_KEY],
+    //   gasPrice: 30000000000, // 30 Gwei
+    // },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    // apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
